32eb7431ef11d08e9275d3e47cb2d9b65bb0ccf0
Michał Grabka <mr.grabka@gmail.com>
2023-04-13 14:52:16 +0200

    update project to angular 16

--- PooPosting/PooPosting.View/src/app/shared/utils/guards/is-not-logged-on-route-guard.guard.ts
+++ PooPosting/PooPosting.View/src/app/shared/utils/guards/is-not-logged-on-route-guard.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
+import { Router } from '@angular/router';
 import {MessageService} from "primeng/api";
 import {AppCacheService} from "../../state/app-cache.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class IsNotLoggedOnRouteGuardGuard implements CanActivate {
+export class IsNotLoggedOnRouteGuardGuard  {
   constructor(
     private cacheService: AppCacheService,
     private messageService: MessageService,
